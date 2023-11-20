d6da0fe8caae811b66133448cec7c1706e020f7b
Hoainguyen2004 <151479921+Hoainguyen2004@users.noreply.github.com>
2023-11-20 15:36:01 +0700

    BMI

--- bmi/App.js
+++ bmi/App.js
@@ -7,16 +7,34 @@ import {
   View,
 } from "react-native";
 
+const clearInput = () => {
+  setWeight("");
+  setHeight("");
+  setBMI(0);
+  setClassification("");
+};
+
 const BMICalculator = () => {
   const [weight, setWeight] = useState("");
   const [height, setHeight] = useState("");
   const [BMI, setBMI] = useState(0);
+  const [classification, setClassification] = useState("");
 
   const computeBMI = () => {
     const weightInKg = parseFloat(weight);
     const heightInM = parseFloat(height) / 100;
     const bmi = weightInKg / (heightInM * heightInM);
     setBMI(bmi);
+
+    if (bmi < 18.5) {
+      setClassification("Under Weight");
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+      setClassification("Normal Weight");
+    } else if (bmi >= 25 && bmi <= 32) {
+      setClassification("Over Weight");
+    } else {
+      setClassification("Obese");
+    }
   };
 
   return (
@@ -35,7 +53,7 @@ const BMICalculator = () => {
         <Text style={styles.label}>Height (cm)</Text>
         <TextInput
           style={styles.input}
-           placeholder="Cm"
+          placeholder="Cm"
           value={height}
           onChangeText={(text) => setHeight(text)}
         />
@@ -43,7 +61,11 @@ const BMICalculator = () => {
       <TouchableOpacity style={styles.button} onPress={computeBMI}>
         <Text style={styles.buttonText}>Compute</Text>
       </TouchableOpacity>
+      <TouchableOpacity style={styles.button} onPress={clearInput}>
+        <Text style={styles.buttonText}>Xóa</Text>
+      </TouchableOpacity>
       <Text style={styles.result}>BMI: {BMI.toFixed(2)}</Text>
+      <Text style={styles.classification}>Classification: {classification}</Text>
     </View>
   );
 };
@@ -92,6 +114,10 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "bold",
     margin: 10,
+  },
+  classification: {
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
 
